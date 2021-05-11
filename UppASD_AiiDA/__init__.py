b4df8d7c7a652036a1b041b32e144fc3baf4b5b5
Qichen Xu <qichenx@kth.se>
2021-05-11 08:32:10 +0200

    init

--- UppASD_AiiDA/__init__.py
+++ UppASD_AiiDA/__init__.py
@@ -1,7 +1,5 @@
 """
-aiida_diff
-
-AiiDA demo plugin that wraps the `diff` executable for computing the difference between two files.
+UppASD-AiiDA interface
 """
 
-__version__ = "1.2.0"
+__version__ = "0.0.1"
